bf4f37209c0eecd7873c6f00e6b4180e471a86f3
Unknown <tcpskid>
2024-03-30 13:16:38 +0530

    Dikha phele

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    const menu = document.querySelector('.menu');
-
-    menuIcon.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
-});
